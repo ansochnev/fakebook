45c010421a09c25b712df418f4ccd9a0d1dd1c43
Anton Sochnev <an.sochnev@yandex.ru>
2022-03-29 01:18:04 +0300

    Add SQL script for database initialization

---
+++ mysql/init.sql
@@ -0,0 +1,39 @@
+CREATE DATABASE fakebook
+    CHARACTER SET utf8;
+
+USE fakebook;
+
+
+CREATE TABLE accounts (
+    id       INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+    email    CHAR(255) NOT NULL,
+    username CHAR(255) NOT NULL
+);
+
+CREATE TABLE passwords (
+    account_id INT UNSIGNED PRIMARY KEY
+        REFERENCES accounts (id)
+            ON UPDATE CASCADE
+            ON DELETE CASCADE,
+
+    hash BINARY(32) NOT NULL,
+    salt BINARY(32) NOT NULL
+);
+
+CREATE TABLE profiles (
+    account_id INT UNSIGNED PRIMARY KEY
+        REFERENCES accounts (id)
+            ON UPDATE CASCADE
+            ON DELETE CASCADE,
+
+    first_name     CHAR(255) NOT NULL,
+    last_name      CHAR(255) NOT NULL,
+    date_of_birth  DATE,
+    sex            ENUM ('female', 'male'),
+    city           CHAR(255),
+    info           TEXT
+);
+
+
+CREATE USER 'fakebook'@'localhost' IDENTIFIED BY 'password';
+GRANT SELECT, INSERT, UPDATE, DELETE ON fakebook.* TO 'fakebook'@'localhost';
